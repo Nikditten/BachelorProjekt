2ba99bbd59b4743a6574a6e20678f84f89916474
Nikoditten <48121115+Nikditten@users.noreply.github.com>
2023-09-26 16:38:18 +0200

    setup

--- test_site/src/pages/_app.tsx
+++ test_site/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import Navigation from "@/components/navigation/navigation";
 import "@/styles/globals.css";
+import { DataCollectorContextProvider } from "@/../../../data_collector/src/index";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Navigation />
+    <Navigation>
       <Component {...pageProps} />
-    </>
+    </Navigation>
   );
 }
