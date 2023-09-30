dc491d9b89f42861f0b14bee322b79618e32b854
Nikoditten <48121115+Nikditten@users.noreply.github.com>
2023-09-30 12:19:44 +0200

    setup

--- test_site/src/analytics/service/DataCollectorContextValue.ts
+++ test_site/src/analytics/service/DataCollectorContextValue.ts
@@ -4,15 +4,14 @@ type DataCollectorProps = {};
 
 export const DataCollectorContextValue = (): DataCollectorProps => {
   useEffect(() => {
-    const href = window.location.href;
-    console.log(href);
+    console.log(window.location.href);
     console.log(new Date().toISOString());
 
     return () => {
       console.log(window.location.href);
       console.log(new Date().toISOString());
     };
-  });
+  }, []);
 
   return {};
 };
